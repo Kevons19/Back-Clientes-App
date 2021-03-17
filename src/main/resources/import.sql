930731f99211bf2f34f66548fcd16bea2cd706b2
Kevin Maldonado <kevin_ms19@outlook.com>
2021-03-17 12:32:23 -0600

    mi primer commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+/*Populate tabla clientes*/
+
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Kevin','Maldonado', 'kevinms@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Eder', 'Rambo', 'eder_rambo@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Enrique', 'Razo', 'erazo@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Fernando', 'Tovar', 'diestroenkiwi@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Jose', 'Ruiz', 'distincion@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mariano', 'Ortega', 'marianosft@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Xiemena', 'Herrera', 'ximenah@outlook.com', '2021-02-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Yannik', 'Hernandez', 'yannikh@outlook.com', '2021-02-19');
